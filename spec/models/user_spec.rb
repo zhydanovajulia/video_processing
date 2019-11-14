c4512434de3d748977cca4ad3c69867d3892afe2
Iuliia Tsaplina <zhydanovajulia@gmail.com>
2019-11-14 12:14:55 +0200

    add model specs to video and user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:user)).to be_valid
+  end
+
+  describe 'validations' do
+
+    it 'is invalid without token' do
+      expect(build(:user, token: nil)).to_not be_valid
+    end
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:video)).to be_valid
+  end
+
+  describe 'validations' do
+
+    it 'is invalid without user id' do
+      expect(build(:video, user_id: nil)).to_not be_valid
+    end
+
+    it 'is invalid without duration' do
+      expect(build(:video, duration: nil)).to_not be_valid
+    end
+
+    it 'is invalid without file_processing' do
+      expect(build(:video, file_processing: nil)).to_not be_valid
+    end
+  end
+
 end
